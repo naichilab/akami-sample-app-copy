0a341ab1fc1a61f4515910b42d1503bac81ec108
akami0000 <konomi0325@gmail.com>
2022-02-19 22:43:47 +0900

    Implement basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,12 @@
 module SessionsHelper
+    # 渡されたユーザーでログインする
     def log_in(user)
         session[:user_id] = user.id
+    end
+    # 現在のユーザーをログアウトする
+    def log_out
+        session.delete(:user_id)
+        @current_user = nil
     end
     def current_user
         if session[:user_id]
